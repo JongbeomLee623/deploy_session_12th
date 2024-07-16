973966bee72db751c516deb23d3b0d028c5ef64f
JongbeomLee623 <jambon23@dgu.ac.kr>
2024-07-16 19:25:45 +0900

    feat: add django-environ setting

--- project/settings.py
+++ project/settings.py
@@ -11,21 +11,23 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -143,7 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # corsheaders
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWE_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     # 로컬 개발용 주소
